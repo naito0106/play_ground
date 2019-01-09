b248fe357c0105be189c13f76ed1962626f2b9af
Yuusuke Naito <naito.yuusuke0322ya@gmail.com>
2019-01-09 13:58:22 +0000

    Install vuex

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -2,11 +2,13 @@
  import Vue from 'vue/dist/vue.esm'
  import App from '../app.vue'
  import router from './router'
+ import { store } from './store'
 
  document.addEventListener('DOMContentLoaded', () => {
    const app = new Vue({
      el: '#sample-app',
      router: router,
+     store,
      render: h => h(App)
    })
  })
---
+++ app/javascript/packs/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue/dist/vue.esm'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    data: {
+      name: '名前'
+    }
+  },
+  mudations: {
+    
+  },
+  actions: {
+    
+  },
+  getters: {
+    
+  }
+})

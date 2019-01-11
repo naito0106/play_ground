61cdc9216ed78402bc0eb1c43681a204cd7219a4
Yuusuke Naito <naito.yuusuke0322ya@gmail.com>
2019-01-11 07:44:12 +0000

    create function using promise

--- app/javascript/router-view/zip-code.vue
+++ app/javascript/router-view/zip-code.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="zip-code">
     <div class="contents">
-      <p @click='test()'>zip</p>
-      {{ $store.state.moduleZipCode.data.message}}
-      <input class="input-zip-code" v-model="zipCode"><button @click="PromiseAddress">住所を取得</button>
-      <p>{{ this.address }}</p>
+       <h3>郵便番号から住所を取得</h3>
+      <input class="input-zip-code" v-model="zipCode"><button @click="fetchAddress">住所を取得</button>
+      <input class="input-address" v-model="address" placeholder="住所1">
+      <input class="input-address" placeholder="住所2">
+      <div class="link-blog">
+        <a href="https://naito-coding0322.hatenablog.com/entry/2019/01/11/163242">ブログ：Promiseの基本</a>
+      </div>
     </div>
-    
   </div>
 </template>
 
@@ -18,39 +20,39 @@ import axios from 'axios';
     data(){
       return {
         zipCode: '',
-        address: 'test',
-        message: 'message'
+        address: ''
       }
     },
     methods: {
-      test(){
-        this.$store.state.moduleZipCode.data.message = 'testメソッド発動';
-      },
-      getAddress(zipCode) {
+      getZipAddressApi(zipCode) {
         return new Promise( function (resolve, reject) {
           axios.get('https://api.zipaddress.net/', {
             params: {
               zipcode: zipCode
             }
-          }).then(res => {
-            if(res.data.code === 200){
-              resolve(res.data.data.fullAddress);
-            } else {
-              reject(new Error(res.data.message));
-            }
-          });
+          })
+            // axiosはリクエストが失敗しない限りFulfilledを返す
+            .then(function (res) {
+              // なのでJSON内のステータスコードで分岐
+              if(res.data.code === 200){
+                resolve(res.data.data.fullAddress);
+              } else {
+                reject(new Error(res.data.message));
+              }
+            });
         });
       },
-       PromiseAddress(){
+      fetchAddress(){
         let zipCode = this.zipCode;
-        let tmp = '';
-        this.getAddress(zipCode).then(function onFulfilled(value){
-          //this.addressがなぜか認識されない
-          console.log(value)
-        }).catch( function onRejected(error){
-          console.error(error);
-
-        })
+        this.getZipAddressApi(zipCode)
+          // 成功時の処理
+          .then(value =>{
+            this.address = value;
+          })
+          // 失敗時の処理
+          .catch(error =>{
+            this.address = '取得に失敗しました'
+          })
       }
     }
   };
@@ -65,9 +67,25 @@ import axios from 'axios';
   align-items: center;  
 }  
 .contents {
+  padding: 30px;
+  box-sizing: border-box;
   background-color: gray;
-
+  float: none;
   width: 500px;
   height: 400px;
+}
+.input-address {
+  width: 350px;
+}
+input {
+  margin: 5px;
+}
+h3 {
+  margin: 0 0 10px 0 ;
+  color: white;
+}
+.link-blog {
+ width: 300px;
+ margin-top: 15px;
 }
 </style>
